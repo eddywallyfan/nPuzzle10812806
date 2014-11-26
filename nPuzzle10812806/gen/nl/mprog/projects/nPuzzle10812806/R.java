8aa18bdc193595f5c0a2828ec2c5def296ac5abd
John Wesselingh <johnwes@live.nl>
2014-11-26 14:12:28 +0100

    Revert "reverse"
    This reverts commit b7566cc88d115cdc14a9f326b4af95631cbd77b4.

--- nPuzzle10812806/gen/nl/mprog/projects/nPuzzle10812806/R.java
+++ nPuzzle10812806/gen/nl/mprog/projects/nPuzzle10812806/R.java
@@ -16,9 +16,6 @@ public final class R {
         public static final int abc_slide_out_bottom=0x7f040004;
         public static final int abc_slide_out_top=0x7f040005;
     }
-    public static final class array {
-        public static final int FOTO=0x7f0c0000;
-    }
     public static final class attr {
         /**  Custom divider drawable to use for elements in the action bar. 
          <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
@@ -1636,10 +1633,7 @@ containing a value of this type.
         public static final int abc_textfield_search_activated_mtrl_alpha=0x7f020030;
         public static final int abc_textfield_search_default_mtrl_alpha=0x7f020031;
         public static final int abc_textfield_search_material=0x7f020032;
-        public static final int hoogwerker=0x7f020033;
-        public static final int ic_launcher=0x7f020034;
-        public static final int knipper=0x7f020035;
-        public static final int zonnigdak=0x7f020036;
+        public static final int ic_launcher=0x7f020033;
     }
     public static final class id {
         public static final int action_bar=0x7f090033;
@@ -1655,12 +1649,12 @@ containing a value of this type.
         public static final int action_mode_bar=0x7f090030;
         public static final int action_mode_bar_stub=0x7f09002f;
         public static final int action_mode_close_button=0x7f090022;
-        public static final int action_settings=0x7f09004a;
+        public static final int action_settings=0x7f090047;
         public static final int activity_chooser_view_content=0x7f090023;
         public static final int always=0x7f090014;
         public static final int beginning=0x7f09001b;
         public static final int bottom=0x7f09001e;
-        public static final int button1=0x7f090045;
+        public static final int button1=0x7f090042;
         public static final int checkbox=0x7f09002b;
         public static final int collapseActionView=0x7f090015;
         public static final int decor_content_parent=0x7f090031;
@@ -1672,21 +1666,18 @@ containing a value of this type.
         public static final int end=0x7f09001c;
         public static final int expand_activities_button=0x7f090024;
         public static final int expanded_menu=0x7f09002a;
-        public static final int gemiddeld=0x7f090047;
-        public static final int gridview=0x7f090041;
+        public static final int gemiddeld=0x7f090044;
         public static final int home=0x7f090000;
         public static final int homeAsUp=0x7f09000e;
         public static final int icon=0x7f090028;
         public static final int ifRoom=0x7f090016;
         public static final int image=0x7f090025;
-        public static final int imageView1=0x7f090044;
-        public static final int label=0x7f090043;
+        public static final int imageView1=0x7f090041;
         public static final int listMode=0x7f09000a;
         public static final int list_item=0x7f090027;
-        public static final int logo=0x7f090042;
-        public static final int makkelijk=0x7f090046;
+        public static final int makkelijk=0x7f090043;
         public static final int middle=0x7f09001d;
-        public static final int moeilijk=0x7f090048;
+        public static final int moeilijk=0x7f090045;
         public static final int never=0x7f090017;
         public static final int none=0x7f09000f;
         public static final int normal=0x7f09000b;
@@ -1710,7 +1701,7 @@ containing a value of this type.
         public static final int split_action_bar=0x7f090004;
         public static final int submit_area=0x7f09003e;
         public static final int tabMode=0x7f09000c;
-        public static final int terug_naar_main=0x7f090049;
+        public static final int terug_naar_main=0x7f090046;
         public static final int title=0x7f090029;
         public static final int top=0x7f09001f;
         public static final int up=0x7f090001;
@@ -1775,10 +1766,10 @@ containing a value of this type.
         public static final int support_simple_spinner_dropdown_item=0x7f03001c;
     }
     public static final class menu {
-        public static final int difficulty=0x7f0d0000;
-        public static final int game_play=0x7f0d0001;
-        public static final int image_selection=0x7f0d0002;
-        public static final int you_win=0x7f0d0003;
+        public static final int difficulty=0x7f0c0000;
+        public static final int game_play=0x7f0c0001;
+        public static final int image_selection=0x7f0c0002;
+        public static final int you_win=0x7f0c0003;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
--- nPuzzle10812806/src/nl/mprog/projects/nPuzzle10812806/ImageSelection.java
+++ nPuzzle10812806/src/nl/mprog/projects/nPuzzle10812806/ImageSelection.java
@@ -5,81 +5,47 @@
 package nl.mprog.projects.nPuzzle10812806;
 
 import android.app.ListActivity;
-import android.content.Context;
 import android.content.Intent;
+//import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.TextView;
-//import android.content.Intent;
 
  
 public class ImageSelection extends ListActivity {
  
+	static final String[] FOTO = new String[] { "Foto 1", "Foto 2", "Foto 3"};
+ 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		String[] FOTO = this.getResources().getStringArray(R.array.FOTO);
+ 
 		setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item,FOTO));
+ 
 		ListView listView = getListView();
 		listView.setTextFilterEnabled(true);
+ 
+		//listView.setOnItemClickListener(new OnItemClickListener() {
+			
+			
 		
-		listView.setOnItemClickListener(new OnItemClickListener() {
-			@Override
-			public void onItemClick(AdapterView<?> arg0, View arg1,int pos, long arg3) {
+ 
+	
+	
+	listView.setOnItemClickListener(new OnItemClickListener() {
+	    @Override
+	    public void onItemClick(AdapterView<?> arg0, View arg1,int pos, long arg3) {
 	        Intent i= new Intent(ImageSelection.this,GamePlay.class);
 	        startActivity(i);
 	        finish();                       
-			}
-		});
-	}
-	public class MobileArrayAdapter extends ArrayAdapter<String> {
-		private final Context context;
-		private final String[] values;
-	 
-		public MobileArrayAdapter(Context context, String[] values) {
-			super(context, R.layout.list_item, values);
-			this.context = context;
-			this.values = values;
-		}
-	 
-		@Override
-		public View getView(int position, View convertView, ViewGroup parent) {
-			LayoutInflater inflater = (LayoutInflater) context
-				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-	 
-			View rowView = inflater.inflate(R.layout.list_item, parent, false);
-			TextView textView = (TextView) rowView.findViewById(R.array.FOTO);
-			ImageView imageView = (ImageView) rowView.findViewById(R.drawable.hoogwerker);
-			textView.setText(values[position]);
-	 
-			// Change icon based on name
-			String s = values[position];
-	 
-			System.out.println(s);
-	 
-			if (s.equals("hoogwerker")) {
-				imageView.setImageResource(R.drawable.hoogwerker);
-			} else if (s.equals("knipper")) {
-				imageView.setImageResource(R.drawable.knipper);
-			} else if (s.equals("zonnigdak")) {
-				imageView.setImageResource(R.drawable.zonnigdak);
-			} else {
-				imageView.setImageResource(R.drawable.ic_launcher);
-			}
-	 
-			return rowView;
-		}
+	    }
+	});}
 	
-	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
