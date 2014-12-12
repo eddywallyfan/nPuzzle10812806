860ccfb8336a7f074b97d07c66761ee4d3aae4b0
eddywallyfan <eddywallyfan@users.noreply.github.com>
2014-12-12 09:11:00 +0100

    Update
    Update

--- nPuzzle10812806/src/nl/mprog/projects/nPuzzle10812806/StartActivity.java
+++ nPuzzle10812806/src/nl/mprog/projects/nPuzzle10812806/StartActivity.java
@@ -1,21 +1,21 @@
 package nl.mprog.projects.nPuzzle10812806;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 
-
-public class MainActivity extends ActionBarActivity {
+public class StartActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.startscherm);
 	}
 
 	public void hoofdmenu (View view) {
-		Intent intent = new Intent (this, ImageSelection.class);
+		Intent intent = new Intent (this, ImageSelectionActivity.class);
 		startActivity(intent);
 		}
 }
+
